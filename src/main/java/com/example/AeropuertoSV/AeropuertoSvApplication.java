7ba5a9e6de561f1c870c6c06a527e92f2e8782b2
grego1986 <joseluisgregoretti@gmail.com>
2023-11-01 12:14:58 -0300

    se crea el proyecto

---
+++ src/main/java/com/example/AeropuertoSV/AeropuertoSvApplication.java
@@ -0,0 +1,13 @@
+package com.example.AeropuertoSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeropuertoSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeropuertoSvApplication.class, args);
+	}
+
+}
